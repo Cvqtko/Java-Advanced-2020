2ca67c175c51d6d83bb63a4a378b3969c5dac7ad
Kiril <Kiril@KirilPC>
2020-03-09 14:57:00 +0200

    Exercises from streams, files and directories

---
+++ 10_streams_files_and_directories/src/Exercise_01.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Exercise_01 {
+	public static void main(String[] args) {
+
+		String path = "input.txt";
+
+		try (BufferedReader reader = Files.newBufferedReader(Paths.get(path))) {
+			String line = reader.readLine();
+
+			while (line != null) {
+				long sum = 0;
+				for (char c : line.toCharArray()) {
+					sum += c;
+				}
+				System.out.println(sum);
+				line = reader.readLine();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ 10_streams_files_and_directories/src/Exercise_02.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Exercise_02 {
+	public static void main(String[] args) {
+
+		String path = "input.txt";
+
+		try (BufferedReader reader = Files.newBufferedReader(Paths.get(path))) {
+			String line = reader.readLine();
+			long sum = 0;
+			while (line != null) {
+				for (char c : line.toCharArray()) {
+					sum += c;
+				}
+				
+				line = reader.readLine();
+			}
+			System.out.println(sum);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
